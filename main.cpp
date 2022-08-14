2ba625f602c295f6747964cf8a11a8507eb67927
unknown <sreenivasmba2015@gmail.com>
2022-08-14 10:19:46 +0530

    initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include<iostream> 
+#include<vector>
+#include<string>
+using namespace std;
+int main()
+{
+    vector<int> v1={10,20,30,4,5};
+    for(auto x:v1)
+    cout<<x<<endl;
+    
+    return 0;
+}
