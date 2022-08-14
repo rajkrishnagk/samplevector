c00b7016b32af24474c0da80185581c89cba76e0
unknown <sreenivasmba2015@gmail.com>
2022-08-14 12:35:26 +0530

    sum of vector elements

--- main.cpp
+++ main.cpp
@@ -1,12 +1,20 @@
 #include<iostream> 
 #include<vector>
 #include<string>
+#include<algorithm>
 using namespace std;
 int main()
 {
     vector<int> v1={10,20,30,4,5};
     for(auto x:v1)
-    cout<<x<<endl;
-    
+    cout<<x;
+
+    int sum=0;
+    cout<<"{";
+    for_each(v1.begin(),v1.end(),[&sum](const auto& x){cout<<x<<"";sum +=x ;});
+
+    cout<<"}"<< endl;
+    cout<<"sum of elements: "<<sum <<endl;
+
     return 0;
 }
